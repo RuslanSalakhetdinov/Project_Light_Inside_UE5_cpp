5043f83f6f0fe586d9c89ac14a7bafc7c7f38fa3
Ruslan Salakhetdinov <79852825377@yandex.ru>
2022-07-30 17:37:04 +0300

    Add project files.

---
+++ Source/LightInside/LightInsideGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LightInsideGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LIGHTINSIDE_API ALightInsideGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
